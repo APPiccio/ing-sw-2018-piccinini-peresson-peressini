6e365d90d46c93be4d4bdc419c1297d4669d7e2c
tompere96 <tompere96@gmail.com>
2018-04-10 17:37:04 +0200

    -Window Panel Class >> Added Constructor that parses from json

--- SagradaPPP/src/main/java/PPP/StaticValues.java
+++ SagradaPPP/src/main/java/PPP/StaticValues.java
@@ -3,7 +3,8 @@ package PPP;
 public class StaticValues {
 
     public static final int NUMBER_OF_DICES_PER_COLOR = 18;
-    public static final int FRONT_SIDE = 0;
-    public static final int BACK_SIDE = 1;
+    public static final int FRONT_SIDE = 1;
+    public static final int BACK_SIDE = 0;
+    public static final String NULL_JSON_VALUE = "null";
 
 }
