a1334c713e2b65fc3d02a887515954806f17b94f
tompere96 <tompere96@gmail.com>
2018-04-10 14:31:29 +0200

    Added Window Panel Class

--- SagradaPPP/src/main/java/PPP/PlayerBoard.java
+++ SagradaPPP/src/main/java/PPP/PlayerBoard.java
@@ -2,6 +2,12 @@ package PPP;
 
 import java.util.ArrayList;
 
+
+
 public class PlayerBoard {
-    private ArrayList<Cell> cells;
+
+    private WindowPanel panel1,panel2;
+    public PlayerBoard() {
+
+    }
 }
