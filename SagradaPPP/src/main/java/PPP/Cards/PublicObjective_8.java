2cec05b412b7dec9af69f177fda950f17a65d4c5
francescoperessini <francesco.peressini@hotmail.com>
2018-04-11 19:23:25 +0200

    Added public objective cards number 1, 5, 6, 7, 8 fix

---
+++ SagradaPPP/src/main/java/PPP/Cards/PublicObjective_8.java
@@ -0,0 +1,37 @@
+package PPP.Cards;
+
+import PPP.StaticValues;
+import PPP.WindowPanel;
+import java.util.Arrays;
+
+//Shade Variety: Sets of one of each value anywhere
+
+public class PublicObjective_8 extends PublicObjective implements PublicObjectiveCard {
+
+    int [] occurrences = {0, 0, 0, 0, 0, 0};
+
+    public int getScore(WindowPanel playerWindowPanel) {
+        for (int i = 0; i < StaticValues.NUMBER_OF_CELLS; i++) {
+            if ((playerWindowPanel.getCellWithIndex(i)).getValue() == 1) {
+                occurrences[0]++;
+            }
+            else if ((playerWindowPanel.getCellWithIndex(i)).getValue() == 2) {
+                occurrences[1]++;
+            }
+            else if ((playerWindowPanel.getCellWithIndex(i)).getValue() == 3) {
+                occurrences[2]++;
+            }
+            else if ((playerWindowPanel.getCellWithIndex(i)).getValue() == 4) {
+                occurrences[3]++;
+            }
+            else if ((playerWindowPanel.getCellWithIndex(i)).getValue() == 5) {
+                occurrences[4]++;
+            }
+            else {
+                occurrences[5]++;
+            }
+        }
+        return Arrays.stream(occurrences).min().getAsInt() * 5;
+    }
+
+}
