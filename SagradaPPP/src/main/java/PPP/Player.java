7d47d1565b68e0609279a2cd74f3102d668ee0ed
Federico Piccinini <federico.piccinini.1@gmail.com>
2018-04-12 23:39:05 +0200

    Starting Game and GameTest implementation. Startin Player implementation

--- SagradaPPP/src/main/java/PPP/Player.java
+++ SagradaPPP/src/main/java/PPP/Player.java
@@ -5,4 +5,20 @@ public class Player {
     private String url;
     private PlayerBoard playerBoard;
 
+    public Player(Player player){
+        this.username = player.getUsername();
+    }
+
+    public Player(String username){
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
 }
