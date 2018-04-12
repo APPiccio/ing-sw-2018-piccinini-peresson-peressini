aa346de46e00dd0dc516780265443f52460c5eca
Federico Piccinini <federico.piccinini.1@gmail.com>
2018-04-12 22:01:01 +0200

    Removing useless class PatternCard, PlayerBoard implementation

--- SagradaPPP/src/main/java/PPP/Player.java
+++ SagradaPPP/src/main/java/PPP/Player.java
@@ -5,13 +5,4 @@ public class Player {
     private String url;
     private PlayerBoard playerBoard;
 
-
-    int getTurnCount(){
-        return 0;
-    } // @return 0 , 1 in base al turno
-    WindowPanel getCurrentWindowPanel(){
-        return null;
-    }
-
-
 }
