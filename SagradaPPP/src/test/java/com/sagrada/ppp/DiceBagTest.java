8e41150404cebfee110a7a098103698a7df9a140
francescoperessini <francesco.peressini@hotmail.com>
2018-04-20 17:06:23 +0200

    Ultimati i test per DiceBag

--- SagradaPPP/src/test/java/com/sagrada/ppp/DiceBagTest.java
+++ SagradaPPP/src/test/java/com/sagrada/ppp/DiceBagTest.java
@@ -2,9 +2,7 @@ package com.sagrada.ppp;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
-import java.util.Random;
 
 public class DiceBagTest {
 
@@ -12,25 +10,30 @@ public class DiceBagTest {
     public static void diceBagTest() {
 
         DiceBag diceBag = new DiceBag();
-        ArrayList<Dice> dices = new ArrayList<>();
-        ArrayList<Dice> newDiceBag;
-        int random = new Random().nextInt(90);
 
         //sizeTest
         assertEquals(90, diceBag.size());
 
-        //numberOfColorTest
-        for (Color color : Color.values()) {
-            assertEquals(18, diceBag.numberOfColor(color));
-        }
+        //extractRandomDiceTest
+        DiceBag i = new DiceBag();
+        DiceBag j = new DiceBag(i);
+
+        assertEquals(i.getDiceBag(), j.getDiceBag());
+        assertNotEquals(i.hashCode(), j.hashCode());
+
+        ArrayList<Dice> k = i.extractDices(10);
+
+        assertNotEquals(i.size(), j.size());
+        assertTrue(j.getDiceBag().containsAll(i.getDiceBag()));
+        assertTrue(j.getDiceBag().containsAll(k));
+        assertEquals(i.size(), j.size() - k.size());
 
         //getDiceBagTest
         assertEquals(diceBag.size(), diceBag.getDiceBag().size());
-        
-        //testing extraction
+
+        //extractDicesTest
         DiceBag x = new DiceBag();
         DiceBag y = new DiceBag(x);
-
         ArrayList<Dice> z = new ArrayList<>();
 
         z.addAll(x.extractDices(10));
@@ -38,8 +41,14 @@ public class DiceBagTest {
         z.addAll(x.extractDices(25));
         z.addAll(x.extractDices(35));
 
-        assertTrue(x.getDiceBag().size() == 0);
+        assertEquals(0, x.getDiceBag().size());
         assertTrue(y.getDiceBag().containsAll(z));
+
+        //numberOfColorTest
+        for (Color color : Color.values()) {
+            assertEquals(18, diceBag.numberOfColor(color));
+        }
+
     }
 
 }
