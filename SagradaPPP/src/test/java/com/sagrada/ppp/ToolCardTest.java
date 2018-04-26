b22d2e3f36dcd85025311ea32818a4f1a04e8c05
Federico Piccinini <federico.piccinini.1@gmail.com>
2018-04-26 14:41:31 +0200

    Merge branch 'master' of https://github.com/APPiccio/ing-sw-2018-piccinini-peresson-peressini
    # Conflicts:
    #	SagradaPPP/src/main/java/com/sagrada/ppp/WindowPanel.java

--- SagradaPPP/src/test/java/com/sagrada/ppp/ToolCardTest.java
+++ SagradaPPP/src/test/java/com/sagrada/ppp/ToolCardTest.java
@@ -18,6 +18,10 @@ public class ToolCardTest {
         card3();
         card4();
         card5();
+        card6();
+        card7();
+        card8();
+        card9();
         card10();
         card11();
         card12();
