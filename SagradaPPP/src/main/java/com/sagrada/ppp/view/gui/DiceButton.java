1716cde70affa9b48a7e8b4df2dcbc2270e79522
tompere96 <tompere96@gmail.com>
2018-05-08 13:45:57 +0200

    add SelectDicePane and DiceButton

---
+++ SagradaPPP/src/main/java/com/sagrada/ppp/view/gui/DiceButton.java
@@ -0,0 +1,31 @@
+package com.sagrada.ppp.view.gui;
+
+import com.sagrada.ppp.Dice;
+import com.sagrada.ppp.utils.StaticValues;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+
+public class DiceButton extends Button {
+
+    public Dice getDice() {
+        return new Dice(dice);
+    }
+
+    private Dice dice;
+    public DiceButton(Dice dice,double diceWidth,double diceHeight) {
+        this.dice = dice;
+        this.setPrefSize(diceWidth,diceHeight);
+        this.setAlignment(Pos.CENTER);
+        this.setBackground(
+                new Background(
+                        new BackgroundImage(
+                                new Image(StaticValues.getAssetUri(dice.getColor(),dice.getValue()),diceWidth,diceHeight,true,true),
+                                BackgroundRepeat.NO_REPEAT,
+                                BackgroundRepeat.NO_REPEAT,
+                                BackgroundPosition.CENTER,
+                                BackgroundSize.DEFAULT)));
+    }
+}
+
---
+++ SagradaPPP/src/main/java/com/sagrada/ppp/view/gui/SelectDicePane.java
@@ -0,0 +1,35 @@
+package com.sagrada.ppp.view.gui;
+
+import com.sagrada.ppp.Dice;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+
+import java.util.ArrayList;
+
+public class SelectDicePane extends GridPane implements EventHandler<MouseEvent> {
+
+    public SelectDicePane(ArrayList<Dice> dices, double diceWidth, double diceHeight ) {
+        this.setVgap(diceHeight/10);
+        this.setHgap(diceWidth/10);
+        int row = 0;
+        int col = 0;
+        for(Dice dice: dices) {
+            DiceButton diceButton = new DiceButton(dice, diceWidth,diceHeight);
+            diceButton.addEventHandler(MouseEvent.MOUSE_CLICKED, this);
+            this.add(diceButton,col,row);
+            if(col < 4){
+                col++;
+            }else {
+                col = 0;
+                row++;
+            }
+        }
+
+    }
+
+    @Override
+    public void handle(MouseEvent event) {
+        System.out.println(((DiceButton)event.getSource()).getDice().toString());
+    }
+}
